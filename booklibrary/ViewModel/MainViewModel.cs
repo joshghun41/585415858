952b339a104e8fba6dd5a0ab3ddd21f947d7f19c
Gulm_be75 <Gulm_be75@STHQ0127-08>
2022-12-06 18:25:24 +0400

    Add project files.

---
+++ booklibrary/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using booklibrary.Commands;
+using booklibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booklibrary.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand StudentCommand { get; set; }
+        public RelayCommand LibrarianCommand { get; set; }
+        public MainWindow mainwindow { get; set; }
+        public MainViewModel()
+        {
+            LibrarianCommand = new RelayCommand(l =>
+            {
+                var librarianWindow = new LibrarianWindow();
+                librarianWindow.Show();
+                mainwindow.Close();
+            });
+            StudentCommand = new RelayCommand(l =>
+            {
+                var studentWindow = new StudentWindow();
+                studentWindow.Show();
+                mainwindow.Close();
+            });
+        }
+    }
+}
